cc1fd3d08f0e9eface2b7e7a3f4acce4ac37dc97
MohammadUvesh-Ghanchi <MohammadUveshMustakbhai.Ghanchi@edu.sait.ca>
2025-09-25 10:02:32 -0600

    Minor changes in Tailwind
    Made changes with components with Tailwind which can make it look good

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,10 +1,10 @@
 
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="bg-slate-800 p-4 mb-4 rounded-lg shadow-md w-full">
+    <li className="bg-slate-800 p-4 mb-4 rounded-lg shadow-md w-full hover:bg-slate-700 transition">
       <h3 className="text-xl font-bold text-white">{name}</h3>
-      <h3 className="text-md text-slate-300">Quantity: {quantity}</h3>
-      <h3 className="text-md text-slate-300">Category: {category}</h3>
+      <p className="text-md text-slate-300">Quantity: {quantity}</p>
+      <p className="text-md text-slate-300">Category: {category}</p>
     </li>
   );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,9 +3,7 @@ import ItemList from "./item-list.js";
 export default function Page() {
   return (
     <main className="bg-slate-950 p-8 flex flex-col items-center">
-      <h1 className="text-4xl font-bold text-white mb-8">
-        Shopping List
-      </h1>
+      <h1 className="text-4xl font-bold text-indigo-400 mb-8">Shopping List</h1>
       <ItemList />
     </main>
   );
