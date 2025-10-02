cdc1cc3e82701aff75c04133106aa9616dd6db2a
MohammadUvesh-Ghanchi <MohammadUveshMustakbhai.Ghanchi@edu.sait.ca>
2025-10-02 14:42:22 -0600

    Changed stying in page.js

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -2,7 +2,7 @@ import NewItem from "./new-item";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-black-100">
       <NewItem />
     </div>
   );
