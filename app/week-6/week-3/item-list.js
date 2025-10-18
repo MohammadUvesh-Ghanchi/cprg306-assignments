764fbf3da887817f6aa92e470cbeafb871a8070a
MohammadUvesh-Ghanchi <MohammadUveshMustakbhai.Ghanchi@edu.sait.ca>
2025-10-18 16:51:07 -0600

    Removed Item array from item-list.js

--- app/week-6/week-3/item-list.js
+++ app/week-6/week-3/item-list.js
@@ -1,68 +1,7 @@
+"use client";
 import Item from "./item.js";
-
-const items = [
-  {
-    name: "milk, 4 L 🥛",
-    quantity: 1,
-    category: "dairy",
-  },
-  {
-    name: "bread 🍞",
-    quantity: 2,
-    category: "bakery",
-  },
-  {
-    name: "eggs, dozen 🥚",
-    quantity: 2,
-    category: "dairy",
-  },
-  {
-    name: "bananas 🍌",
-    quantity: 6,
-    category: "produce",
-  },
-  {
-    name: "broccoli 🥦",
-    quantity: 3,
-    category: "produce",
-  },
-  {
-    name: "chicken breasts, 1 kg 🍗",
-    quantity: 1,
-    category: "meat",
-  },
-  {
-    name: "pasta sauce 🍝",
-    quantity: 3,
-    category: "canned goods",
-  },
-  {
-    name: "spaghetti, 454 g 🍝",
-    quantity: 2,
-    category: "dry goods",
-  },
-  {
-    name: "toilet paper, 12 pack 🧻",
-    quantity: 1,
-    category: "household",
-  },
-  {
-    name: "paper towels, 6 pack",
-    quantity: 1,
-    category: "household",
-  },
-  {
-    name: "dish soap 🍽️",
-    quantity: 1,
-    category: "household",
-  },
-  {
-    name: "hand soap 🧼",
-    quantity: 4,
-    category: "household",
-  },
-];
-
+import { useState } from "react";
+import item from "item.json";
 export default function ItemList() {
   return (
     
